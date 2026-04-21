4c023587f4cc2c30fc8cc76a9a2fe679ade8c6b9
daisuke.miyoshi <nana40481023@gmail.com>
2026-04-21 13:17:43 +0900

    fix: パディング調整

--- annotation/clipping.py
+++ annotation/clipping.py
@@ -10,20 +10,17 @@ def clipping(x_center, y_center, z_center, slice_folder_path):
         dicom = pydicom.dcmread(os.path.join(slice_folder_path, filename))
         slice_dicoms.append(dicom)
 
-    # 30*30*30の配列を作る
-    voxels = np.zeros((30, 30, 30, 1), dtype=np.uint16)
+    voxels = np.zeros((32, 32, 32, 1), dtype=np.uint16)
 
     # 各スライス画像を処理し、ボクセルデータに変換する
-    for z in range(0, 30):
-        slice_dicom = slice_dicoms[z + z_center - 15]
+    for z in range(0, 32):
+        slice_dicom = slice_dicoms[z + z_center - 16]
         slice_dicom_pixel = slice_dicom.pixel_array
-        for x in range(0, 30):
-            for y in range(0, 30):
-                voxels[x][y][z] = slice_dicom_pixel[x + x_center - 15][y + y_center - 15]
+        for x in range(0, 32):
+            for y in range(0, 32):
+                voxels[x][y][z] = slice_dicom_pixel[x + x_center - 16][y + y_center - 16]
 
     # conv3dで使いやすくするために，ボクセルデータを正規化,転置する
-    # voxels = voxels / 1500
-    # voxels = voxels.T
-    # print(voxels.dtype)
-    # ボクセルデータをnpyファイルとして保存する
+    voxels = (voxels - voxels.min()) / (voxels.max() - voxels.min())
+    voxels = voxels.T
     np.save(f"./{os.path.basename(slice_folder_path)}_pl.npy", voxels)
